d7e57da90b26c9ae2df814f42fbaad5eb3a0e58d
AndreCorreaSantos <acorreasantos2002@gmail.com>
2024-09-03 18:38:08 -0300

    adding client and server logic

---
+++ client/Assets/client/Connection.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NativeWebSocket;
+
+public class Connection : MonoBehaviour
+{
+  WebSocket websocket;
+
+  // Start is called before the first frame update
+  public string url = "ws://localhost:8000";
+  async void Start()
+  {
+    websocket = new WebSocket(url);
+
+    websocket.OnOpen += () =>
+    {
+      Debug.Log("Connection open!");
+    };
+
+    websocket.OnError += (e) =>
+    {
+      Debug.Log("Error! " + e);
+    };
+
+    websocket.OnClose += (e) =>
+    {
+      Debug.Log("Connection closed!");
+    };
+
+    websocket.OnMessage += (bytes) =>
+    {
+      Debug.Log("OnMessage!");
+      Debug.Log(bytes);
+
+      // getting the message as a string
+      // var message = System.Text.Encoding.UTF8.GetString(bytes);
+      // Debug.Log("OnMessage! " + message);
+    };
+
+    await websocket.Connect();
+  }
+
+  void Update()
+  {
+    #if !UNITY_WEBGL || UNITY_EDITOR
+      websocket.DispatchMessageQueue();
+    #endif
+  }
+
+  public async void SendWebSocketMessage(byte[] message)
+  {
+    if (websocket.State == WebSocketState.Open)
+    {
+      await websocket.Send(message);
+    }
+  }
+
+  private async void OnApplicationQuit()
+  {
+    await websocket.Close();
+  }
+
+}
